1cb1eac3051b16f75339d29ee387f485ac277f32
Anonymous <>
2022-03-01 08:55:30 +1300

    Update

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use eframe::{run_native, epi::App, egui::CentralPanel, NativeOptions};
 
+struct Headlines;
+
+impl App for Headlines {
+    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("article text");
+        });
+    }
+
+    fn name(&self) -> &str {
+        "Headlines"
+    }
+}
+
+fn main() {
+    let app = Headlines;
+    let win_option = NativeOptions::default();
+    run_native(Box::new(app), win_option);
 }
